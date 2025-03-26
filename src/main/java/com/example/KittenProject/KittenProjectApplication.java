64d1fef9e569b770396d784687e0627acfef183c
Rahul Raj N <rrajn2392@gmail.com>
2025-03-26 17:49:57 +0530

    initial commit

---
+++ src/main/java/com/example/KittenProject/KittenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.KittenProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KittenProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KittenProject/KittenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KittenProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KittenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
